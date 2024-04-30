578d9e296f99ee005c0676a32a872d22d8add927
Josh <josh.mayfield45@gmail.com>
2024-04-30 19:30:36 -0400

    added classnames to Profile.jsx

--- react-portfolio/src/components/Profile/Profile.jsx
+++ react-portfolio/src/components/Profile/Profile.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
-import styles from '.'
+import styles from './Profile.module.css';
 import { getImage } from '../../utils';
 
 export const Hero = () => {
-  return <section>
-    <div>
-      <h1>Welcome to my Portfolio!</h1>
-      <p>I'm a full stack developer with experience usiing React and NodeJS.</p>
-      <a href="mailto:josh.mayfield45@gmail.com"></a>
+  return (
+  <section className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Welcome to my Portfolio!</h1>
+      <p className={styles.description}>
+        I'm a full stack developer with experience usiing React and NodeJS.
+      </p>
+      <a className={styles.contactBtn} href="mailto:josh.mayfield45@gmail.com">
+        Contact Me
+      </a>
     </div>
-    <img src={getImage("profile-image/profile.png")} alt="profile image" />
+    <img 
+      className={styles.profileImg} 
+      src={getImage("profile-image/profile.png")} 
+      alt="profile image" 
+    />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
-  </section>;
-
+  </section>
+  );
 };
