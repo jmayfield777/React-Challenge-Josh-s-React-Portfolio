fb59f562be431b90c76de6373901b51bd25ea7b8
Josh <josh.mayfield45@gmail.com>
2024-04-30 18:17:42 -0400

    updated index.jsx in navbar component

--- react-portfolio/src/components/Navbar/index.jsx
+++ react-portfolio/src/components/Navbar/index.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return <nav>
+    <a href="/">Josh's Portfolio</a>
+    <div>
+      <ul>
+        <li><a href="#About">About</a></li>
+      </ul>
+      <ul>
+        <li><a href="#Projects">Projects</a></li>
+      </ul>
+      <ul>
+        <li><a href="#Resume">Resume</a></li>
+      </ul>
+      <ul>
+        <li><a href="#Contact">Contact</a></li>
+      </ul>
+    </div>
+  </nav>;
 };
