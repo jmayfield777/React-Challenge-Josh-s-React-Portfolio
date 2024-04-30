d421363203c769b6ecfd80b3cb7807d9803558f7
Josh <josh.mayfield45@gmail.com>
2024-04-30 17:18:54 -0400

    updated About page index.jsx

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,14 @@
+function About () {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+      Josh Mayfield is an aspiring full stack web developer 
+      based in Brooklyn, NY. He is currently learning HTML, 
+      CSS, and Javascript. He hopes to be able to use this 
+      knowledge to secure a job in the industry and also learn 
+      learn other languages like C+ for creative purposes.
+      </p>
+    </div>
+  );
+}
