e5b0128a039849aedd8f3e2a68e9935f7713db45
Josh <josh.mayfield45@gmail.com>
2024-04-30 18:37:12 -0400

    updated index.jsx in navbar component

--- react-portfolio/src/components/Navbar/index.jsx
+++ react-portfolio/src/components/Navbar/index.jsx
@@ -7,16 +7,18 @@ export const Navbar = () => {
     <a className={styles.title} href="/">Josh's Portfolio</a>
     <div className={styles.menu}>
       <ul className={styles.menuItems}>
-        <li><a href="#About">About</a></li>
-      </ul>
-      <ul>
-        <li><a href="#Projects">Projects</a></li>
-      </ul>
-      <ul>
-        <li><a href="#Resume">Resume</a></li>
-      </ul>
-      <ul>
-        <li><a href="#Contact">Contact</a></li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#Projects">Projects</a>
+        </li>
+        <li>
+          <a href="#Resume">Resume</a>
+        </li>
+        <li>
+          <a href="#Contact">Contact</a>
+        </li>
       </ul>
     </div>
   </nav>;
