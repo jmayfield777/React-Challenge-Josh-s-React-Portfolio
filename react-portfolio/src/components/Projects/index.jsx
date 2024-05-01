937e7646c1aaed0083b26812333e3f37ae53b206
Josh <josh.mayfield45@gmail.com>
2024-04-30 22:38:29 -0400

    updated index.jsx in Projects component

--- react-portfolio/src/components/Projects/index.jsx
+++ react-portfolio/src/components/Projects/index.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+import projects from '../../data/projects.json';
+import { getImage } from '../../utils/getImageHelper';
+
 export const Projects = () => {
   return (
-    <div>Projects</div>
+    <section>
+      <h2>Projects</h2>
+      <div>
+        {
+          projects.map((project, id) => {
+            return (
+              <div key={id}>
+                <img 
+                  src={getImage(project.imageSrc)} 
+                  alt={`Image of ${project.title}`} 
+                />
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <ul>
+                  {
+                    project.skills.map((skill, id) => {
+                      <li key={id}>{skill}</li>;
+                    })}
+                </ul>
+                <div></div>
+                  <a href={project.demo}>Demo</a>
+                  <a href={project.source}>Source</a>
+            </div>
+            );
+          })}
+      </div>
+    </section>
   );
 };
