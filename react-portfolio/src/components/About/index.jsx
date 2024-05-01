d49527800386becebbc78f1006b0bd1a34a0dab2
Josh <josh.mayfield45@gmail.com>
2024-04-30 21:04:23 -0400

    added classnames to index.jsx in the about componenet

--- react-portfolio/src/components/About/index.jsx
+++ react-portfolio/src/components/About/index.jsx
@@ -5,40 +5,40 @@ import { getImage } from '../../utils/getImageHelper';
 
 export const About = () => {
   return (
-  <section>
-    <h2>About</h2>
-    <div>
-      <ul>
-        <li>
+  <section className={styles.container}>
+    <h2 className={styles.title}>About</h2>
+    <div className={styles.content}>
+      <ul className={styles.aboutItems}>
+        <li className={styles.aboutItem}>
           <img 
             src={getImage("About/cursorIcon.png")} 
             alt="cursor icon" 
           />
-          <div>
+          <div className={styles.aboutItemText}>
             <h3>Frontend Developer</h3>
             <p>
               I'm a frontend developer with experience in building optimized sites.
             </p>
           </div>
         </li>
-        <li>
+        <li className={styles.aboutItem}>
           <img 
             src={getImage("About/serverIcon.png")} 
             alt="server icon" 
           />
-          <div>
+          <div className={styles.aboutItemText}>
             <h3>Backend Developer</h3>
             <p>
               I have experience developing fast and optimized backend systems and API's.
             </p>
           </div>
         </li>
-        <li>
+        <li className={styles.aboutItem}>
           <img 
             src={getImage("About/uliIcon.png")} 
-            alt="uli icon" 
+            alt="UI icon" 
           />
-          <div>
+          <div className={styles.aboutItemText}>
             <h3>UI Designer</h3>
             <p>
               I have designed multiple landing pages and have created design systems as well.
