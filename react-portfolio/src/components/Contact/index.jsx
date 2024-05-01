fe8667f09df0d9cb1a8ed02711d60fc9b66d0236
Josh <josh.mayfield45@gmail.com>
2024-05-01 00:05:31 -0400

    updated index.jsx in Contact component

--- react-portfolio/src/components/Contact/index.jsx
+++ react-portfolio/src/components/Contact/index.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { getImage } from "../../utils/getImageHelper";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <footer>
+      <div>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul>
+        <li>
+          <img 
+            src={getImage("Contact/emailIcon.png")} 
+            alt="Email icon" 
+          />
+          <a href="mailto:josh.mayfield45@gmail.com">
+            josh.mayfield45@gmail.com
+          </a>
+        </li>
+        <li>
+          <img 
+            src={getImage("Contact/githubIcon.png")} 
+            alt="Github icon" 
+          />
+          <a href="https://www.github.com/jmayfield777">
+            github.com/jmayfield777
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
