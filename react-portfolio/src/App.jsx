3a5650656e5658ec5142731fd99969941b18fc19
Josh <josh.mayfield45@gmail.com>
2024-04-30 21:16:56 -0400

    imported about component to App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.css';
 import { Navbar } from './components/Navbar';
 import { Profile } from './components/Profile';
+import { About } from './components/About';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className={styles.App}>
       <Navbar />
       <Profile />
+      <About />
     </div>
   )
 }
