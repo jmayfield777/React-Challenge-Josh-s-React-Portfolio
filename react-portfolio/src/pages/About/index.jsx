9774882959494385acfbbd94d0b48dec87983929
Josh <josh.mayfield45@gmail.com>
2024-05-02 19:44:02 -0400

    updated About index.jsx and index.module.css in pages

---
+++ react-portfolio/src/pages/About/index.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import styles from './index.module.css';
+import { getImage } from '../../utils/getImageHelper';
+
+export const About = () => {
+  return (
+  <section className={styles.container} id="about">
+    <h2 className={styles.title}>About</h2>
+    <div className={styles.content}>
+      <ul className={styles.aboutItems}>
+        <li className={styles.aboutItem}>
+          <img 
+            src={getImage("About/cursorIcon.png")} 
+            alt="cursor icon" 
+          />
+          <div className={styles.aboutItemText}>
+            <h3>Frontend Developer</h3>
+            <p>
+              I'm a frontend developer with experience in building optimized sites.
+            </p>
+          </div>
+        </li>
+        <li className={styles.aboutItem}>
+          <img 
+            src={getImage("About/serverIcon.png")} 
+            alt="server icon" 
+          />
+          <div className={styles.aboutItemText}>
+            <h3>Backend Developer</h3>
+            <p>
+              I have experience developing fast and optimized backend systems and API's.
+            </p>
+          </div>
+        </li>
+        <li className={styles.aboutItem}>
+          <img 
+            src={getImage("About/uiIcon.png")} 
+            alt="UI icon" 
+          />
+          <div className={styles.aboutItemText}>
+            <h3>UI Designer</h3>
+            <p>
+              I have designed multiple landing pages and have created design systems as well.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+  );
+}
