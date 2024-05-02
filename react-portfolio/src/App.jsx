6b19552e6ff9b26f0b3d08c0c56b88ba7d32c0db
Josh <josh.mayfield45@gmail.com>
2024-05-02 19:40:02 -0400

    imported footer into App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,10 +3,12 @@ import { Outlet } from 'react-router-dom';
 import styles from './App.module.css';
 
 import { Navbar } from './components/Navbar';
-import { Profile } from './components/Profile';
-import { About } from './components/About';
-import { Projects } from './components/Projects';
-import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+
+// import { Profile } from './components/Profile';
+// import { About } from './components/About';
+// import { Projects } from './components/Projects';
+// import { Contact } from './components/Contact';
 
 // function App() {
 
@@ -27,6 +29,7 @@ function App() {
     <div className={styles.App}>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
