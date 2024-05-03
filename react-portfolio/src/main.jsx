72ec9ec63d978f42bb42f2035905a148b5ad6b53
Josh <josh.mayfield45@gmail.com>
2024-05-02 20:33:27 -0400

    addedimports and error element to main.jsx

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx'
-import ErrorPage from './pages/ErrorPage';
-import About from './pages/About.jsx';
-import Projects from './pages/Projects.jsx';
-import Resume from './pages/Resume.jsx';
+import ErrorPage from './pages/ErrorPage/index.jsx';
+import Profile from './pages/Profile/index.jsx';
+import About from './pages/About/index.jsx';
+import Projects from './pages/Projects/index.jsx';
+import Resume from './pages/Resume/index.jsx';
 
 import './index.css'
 
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
