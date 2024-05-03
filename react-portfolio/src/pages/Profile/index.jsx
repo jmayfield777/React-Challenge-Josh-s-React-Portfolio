b98f28809bb10b0cdbfeed23a9bdf3768acb1e75
Josh <josh.mayfield45@gmail.com>
2024-05-02 20:24:29 -0400

    added and updated index.jsx and index.module.css in Profile page

---
+++ react-portfolio/src/pages/Profile/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from './index.module.css';
+import { getImage } from '../../utils/getImageHelper';
+
+export const Profile = () => {
+  return (
+  <section className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Welcome to my Portfolio!</h1>
+      <p className={styles.description}>
+        I'm a full stack developer with experience usiing React and NodeJS.
+      </p>
+      <a className={styles.contactBtn} href="mailto:josh.mayfield45@gmail.com">
+        Contact Me
+      </a>
+    </div>
+    <img 
+      className={styles.profileImg} 
+      src={getImage("Profile-Image/Profile.png")} 
+      alt="profile image" 
+    />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+  </section>
+  );
+};
