02f38b2e14dc16c7790f85e0f7049e2786b9032e
Josh <josh.mayfield45@gmail.com>
2024-05-02 20:29:13 -0400

    updated App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -6,25 +6,6 @@ import styles from './App.module.css';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 
-// import { Profile } from './components/Profile';
-// import { About } from './components/About';
-// import { Projects } from './components/Projects';
-// import { Contact } from './components/Contact';
-
-// function App() {
-
-
-//   return (
-//     <div className={styles.App}>
-//       <Navbar />
-//       <Profile />
-//       <About />
-//       <Projects />
-//       <Contact />
-//     </div>
-//   )
-// }
-
 function App() {
   return (
     <div className={styles.App}>
